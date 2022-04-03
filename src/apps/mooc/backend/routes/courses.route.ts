947e006d0aee689860e042cf7d46d310ff2203b9
Rubén Ruiz <rubenruizpedreira@gmail.com>
2022-04-04 01:28:18 +0200

    validaciones

--- src/apps/mooc/backend/routes/courses.route.ts
+++ src/apps/mooc/backend/routes/courses.route.ts
@@ -1,7 +1,18 @@
 import { Router, Request, Response } from 'express';
+import { body } from 'express-validator';
 import container from '../dependency-injection';
 
+import { validateReqSchema } from '.';
+
 export const register = (router: Router) => {
-  const coursePutController = container.get('Apps.mooc.controllers.CoursePutController');
-  router.put('/courses/:id', (req: Request, res: Response) => coursePutController.run(req, res));
+	const reqSchema = [
+		body('id').exists().isString(),
+		body('name').exists().isString(),
+		body('duration').exists().isString()
+	];
+
+	const coursePutController = container.get('Apps.mooc.controllers.CoursePutController');
+	router.put('/courses/:id', reqSchema, validateReqSchema, (req: Request, res: Response) =>
+		coursePutController.run(req, res)
+	);
 };
